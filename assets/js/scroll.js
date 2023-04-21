8e3cd6ae7069e95037fbc28c3d7e8043d89fadd3
Bleydson.Mendonça <bleydsonanderson@hotmail.com>
2023-04-21 15:11:40 -0300

    mais algumas alterações

--- assets/js/scroll.js
+++ assets/js/scroll.js
@@ -1,6 +1,6 @@
 window.sr = ScrollReveal({ reset:true});
 ScrollReveal({ distance: '15px' });
-sr.reveal('.desenho', {duration : 3000, origin: 'right'})
+sr.reveal('.quadroimg', {duration : 3000, origin: 'right'})
 sr.reveal('.descricao', {duration : 3000, origin: 'left'})
 sr.reveal('.linkedin', {duration : 3000,  origin: 'left'})
 sr.reveal('.sobre-mim', {duration : 3000, origin:'left'})
